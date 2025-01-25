70687e4bc57d80108947864a1c232d5a86366c75
Duken Marga <dukenmarga@gmail.com>
2025-01-25 23:43:07 +0900

    frontend: append chat input as history and add send button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { Textarea } from '$lib/components/ui/textarea/index.js';
+	import SendIcon from 'lucide-svelte/icons/send';
 
 	let input: HTMLTextAreaElement = document.createElement('textarea');
 	let history: HTMLDivElement;
@@ -12,6 +13,32 @@
 		if (input && input.style) {
 			input.style.height = 'auto';
 			input.style.height = input.scrollHeight + 'px';
+		}
+	}
+	function handleKeydown(event: KeyboardEvent) {
+		if (input === null) return;
+		if (event.key === 'Enter' && event.ctrlKey && event.shiftKey) {
+			// Prevents a new line in textarea
+			event.preventDefault();
+
+			// get text from textarea
+			const prevCommand = input.value;
+
+			// clear textarea
+			input.value = '';
+
+			// create new div before the textarea
+			const histLine = document.createElement('div');
+			histLine.innerHTML = `
+				<div class="grid grid-cols-6 pb-4">
+                    <div class="col-span-5 col-start-2 rounded-xl bg-gray-100 p-4">
+                        ${prevCommand}
+                    </div>
+                </div>
+			`;
+
+			// add new div before the textarea
+			history.appendChild(histLine);
 		}
 	}
 </script>
@@ -43,16 +70,22 @@
 			</div>
 		</div>
 		<div id="input" class="grid grid-cols-12 p-4">
-			<div class="col-span-8 col-start-3 rounded-xl bg-gray-100 p-4">
+			<div class="col-span-8 col-start-3 rounded-xl bg-gray-100 p-2">
 				<div class="">
 					<Textarea
 						placeholder="Message ..."
-						class="resize-none border-none"
+						class="resize-none border-none shadow-none focus-visible:ring-0"
 						style="height: 50px;"
 						bind:ref={input}
 						oninput={resize}
+						onkeydown={handleKeydown}
 					/>
-					<div class="icon"></div>
+					<div class="icon">
+						<div class="float-left"></div>
+						<div class="float-right mb-1 mr-1 rounded-full bg-gray-800 p-2 text-white">
+							<SendIcon class="size-5" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
