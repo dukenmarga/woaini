4ab23766cb0400509fc27d7e9aa1705eb95ae3e5
Duken Marga <dukenmarga@gmail.com>
2025-01-25 17:24:53 +0900

    frontend: add sidebar and user interface skeleton

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,15 +10,8 @@
 		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
 			<Sidebar.Trigger class="-ml-1" />
 			<Separator orientation="vertical" class="mr-2 h-4" />
-			Title here
+			New chat
 		</header>
-		<div class="flex flex-1 flex-col gap-4 p-4">
-			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-				<div class="bg-muted/50 aspect-video rounded-xl"></div>
-				<div class="bg-muted/50 aspect-video rounded-xl"></div>
-				<div class="bg-muted/50 aspect-video rounded-xl"></div>
-			</div>
-			<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
-		</div>
+		<div class="flex flex-1 flex-col gap-4 p-4"></div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
